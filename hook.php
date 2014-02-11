aa2ee56c28f2dc51144d40c14ee10cbcb3f3fc50
Ben Verhees <b.verhees@colours.nl>
2014-02-11 15:49:27 +0100

    Check existing commit

--- hook.php
+++ hook.php
@@ -14,19 +14,22 @@ if(isset($_POST["payload"])){
 	$payload = json_decode($_POST["payload"]);
 	$ref = isset($payload->ref) ? $payload->ref : "";
 	$result['ref'] = $ref;
+	$tag = strstr($ref, 'refs/tags');
+	$result['tag'] = $tag;
 	$base_ref = isset($payload->base_ref) ? $payload->base_ref : "";
 	$result['base_ref'] = $base_ref;
 	$head_commit_id = (isset($payload->head_commit) && isset($payload->head_commit->id)) ? $payload->head_commit->id : "";
 	$result['head_commit_id'] = $head_commit_id;
 
 	if(!empty($ref)) {
-		$result['fetch'] = shell_exec($git . ' fetch 2>&1');
+		$result['fetch'] = shell_exec($git . ' fetch --tags 2>&1');
 		$result['show_ref'] = shell_exec(sprintf($git . ' show-ref --tags -s %s 2>&1', $ref));
-		
-		if($head_commit_id == $result['show_ref']) {
-			$result['checkout'] = shell_exec(sprintf($git . ' checkout %s 2>&1', $ref));
-		}
+		$result['show_ref'] = trim($result['show_ref']);
+		//if($head_commit_id == $result['show_ref']) {
+		//	$result['checkout'] = shell_exec(sprintf($git . ' checkout %s 2>&1', $ref));
+		//}
 	}
+
 }
 
 header('Content-Type: application/json');
